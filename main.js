a70c2f4298bd61261b370fb43ba8229e54edb68e
Santiago Gonzalez <punigonzalez07@gmail.com>
2023-02-23 13:49:21 -0300

    se agrega el proyecto

---
+++ main.js
@@ -0,0 +1,5 @@
+let nombreUsuario = prompt ("Ingrese su nombre")
+subtitulo.innerText = `Bienvenido ${nombreUsuario}, elegi el tipo de operacion que deseas realizar`
+
+while (isNaN(nombreUsuario)){
+    nombreUsuario= parseInt(prompt ("Escribi tu nombre"))}
